df2ce0b74a15e4abab56a94dab5bb59870ef87cb
kesa <ksahithi955@gmail.com>
2025-06-15 15:15:59 +0530

    adding lots to gitignore

--- aws/compute/tf-ec2/simple-instance/main.tf
+++ aws/compute/tf-ec2/simple-instance/main.tf
@@ -1,3 +1,14 @@
 provider "aws"{
-    region = 'us-east-1'
+    region = "ap-south-1"
+}
+
+resource "aws_instance" "tf-test-1" {
+    instance_type = "t2.micro"
+    subnet_id = "subnet-06f9a6abaf21580a1"
+    ami = "ami-0b09627181c8d5778"
+    vpc_security_group_ids = ["sg-03e8f52d3601826be"]
+    tags = {
+        Name   = "tf-test-1"
+        Environment = "test"
+  }
 }
